20ee1d1a537cf24c015ac80e796edffdcf67b1d6
Namrata Shirsat <namrata.shirsaat@gmail.com>
2024-07-03 19:49:32 +0530

    Update index.js
    adding another method of split, reverse and join for palindrome project

--- index.js
+++ index.js
@@ -3,23 +3,42 @@ const check = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 function validityofinput() {
-  const inputValue = input.value.toString();
-  const inputLength = inputValue.length;
-//   inputValue.style.fontWeight ="900"; 
-  if (inputLength === 0) {
-    alert("Please enter value");
-  }
-   else {
-    for (i = 0; i < inputLength / 2; i++) {
-      if (inputValue[i] !== inputValue[inputLength - 1 - i]) {
-        result.innerHTML = "<b>"+ inputValue+"</b>"+"it is not a palindrome";
-      }
-      else{
-        result.innerHTML ="<b>"+ inputValue+"</b>"+ " it is a palindrome";
-
-      }
+  // using split and revese function
+  const actulaValue = input.value;
+  const splitvalue = input.value.split("");
+  const reverse = splitvalue.reverse().join("");
+  // console.log(reverse);
+  // console.log(splitvalue[1]);
+  if (actulaValue.length === 0) {
+    alert("Please input a value");
+  } else {
+    if (actulaValue === reverse) {
+      result.innerHTML = "<b>" + input.value + "</b>" + " it is a palindrome";
+    } else {
+      result.innerHTML =
+        "<b>" + input.value + "</b>" + " it is not a palindrome";
     }
-    
-    // console.log(inputValue[i]);
   }
+
+  // using length and array
+
+  // const inputValue = input.value.toString();
+  // const inputLength = inputValue.length;
+
+  // console.log(inputValue);
+  // console.log(inputLength);
+
+  // if (inputLength === 0) {
+  //   alert("Please input a value");
+  // } else {
+  //   for (i = 0; i < inputLength; i++) {
+  //     // console.log(inputValue[i]);
+  //     if (inputValue[i] != inputValue[inputLength -1- i]) {
+  //       result.innerHTML =
+  //         "<b>" + inputValue + "</b>" + " it is not a palindrome";
+  //     }
+  //     else {
+  //       result.innerHTML = "<b>" + inputValue + "</b>" + " it is a palindrome";
+  //     }
+  //   }
 }
